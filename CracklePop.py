96cf70180b450259c34e8dd9bbf0f03eaee9e8fa
Eddie <86079470+esand68@users.noreply.github.com>
2022-04-01 16:12:26 -0400

    Add files via upload

---
+++ CracklePop.py
@@ -0,0 +1,15 @@
+
+# counts from 1 to 100 inclusive and checks if number is dvisible by 3 and/or 5
+for i in range(1,101):
+
+  if i%3==0 and i%5==0:
+    print('CracklePop')
+
+  elif i%3==0:
+    print('Crackle')
+
+  elif i%5==0:
+    print('Pop')
+
+  else:
+      print(i)
